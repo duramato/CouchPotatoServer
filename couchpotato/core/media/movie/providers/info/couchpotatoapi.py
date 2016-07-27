7634f23ae0a46082e4fdce15a43a3cc295340a86
duramato <matigonkas@outlook.com>
2016-07-27 18:38:08 +0100

    Fix

--- couchpotato/core/media/movie/providers/info/couchpotatoapi.py
+++ couchpotato/core/media/movie/providers/info/couchpotatoapi.py
@@ -109,7 +109,7 @@ class CouchPotatoApi(MovieProvider):
         #http://www.omdbapi.com/ http://videoeta.com/ http://stackoverflow.com/questions/9321947/how-to-get-dvd-release-dates-of-movies-programatically 
         #https://www.themoviedb.org/documentation/api
         dates = self.getJsonData(self.urls['eta'] % identifier, headers = self.getRequestHeaders())
-        log.debug('Found ETA for %s: %s', (identifier, dates, headers))
+        log.debug('Found ETA for %s: %s', (identifier, dates))
 
         return dates
 
